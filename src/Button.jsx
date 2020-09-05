0a2ff3ab23c9874fed1b7843a82ada7c59f34c35
avremi gersuni <amgershuni@gmail.com>
2020-09-06 01:04:54 +0300

    first step at npm library

---
+++ src/Button.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = ({
+  backgroundColor,
+  borderColor,
+  justifyContent,
+  text,
+  functionToDisplay,
+}) => {
+  return (
+    <MyButton
+      bgColor={backgroundColor}
+      brColor={borderColor}
+      jc={justifyContent}
+      onClick={() => {
+        functionToDisplay();
+      }}
+    >
+      {text}
+    </MyButton>
+  );
+};
+export default Button;
+
+const MyButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 35px;
+  width: 80px;
+  align-items: center;
+  border-radius: 8px;
+  cursor: pointer;
+  border: 2px ${(props) => (props.brColor ? props.brColor : "blue")} solid;
+  background-color: ${(props) => (props.bgColor ? props.bgColor : "yellow")};
+  justify-content: ${(props) => (props.jc ? props.jc : "flex-start")};
+`;
+/* height: ${height}; */
